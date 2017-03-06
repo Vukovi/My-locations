58d1388febbce54afdbba3afc208f7fcd8c33e21
Vukovi <Vuk@Users-MacBook-Pro.local>
2017-03-07 00:00:53 +0100

    a

--- opet moje lokacije/TableViewController.swift
+++ opet moje lokacije/TableViewController.swift
@@ -8,88 +8,146 @@
 
 import UIKit
 
+var izabraniRedTabele: Int?
+var nizGeografskihDužina = [String]()
+var nizGeografskihŠirina = [String]()
+var nizAdresaUlica = [String]()
+var nizAdresaBrojeva = [String]()
+
 class TableViewController: UITableViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if nizGeografskihDužina.count == 0 {
+            return 0
+        }
+        else{
+            return nizGeografskihDužina.count
+        }
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mojaCelija", for: indexPath)
+        if nizAdresaUlica[indexPath.row] != "" {
+            cell.textLabel?.text = nizAdresaUlica[indexPath.row] + " " + nizAdresaBrojeva[indexPath.row]
+        }
+        else{
+            cell.textLabel?.text = "Neznana lokacija!"
+        }
+        let dugoPritisnutaĆeliija = UILongPressGestureRecognizer(target: self, action: #selector(TableViewController.stisniĆeliju(_:)))
+        cell.addGestureRecognizer(dugoPritisnutaĆeliija)
 
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        izabraniRedTabele = indexPath.row
+        return indexPath
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    var niz1 = [String]()
+    var niz2 = [String]()
+    func stisniĆeliju(_ prepoznaj:UILongPressGestureRecognizer){
+        tableView.isUserInteractionEnabled = true
+        if prepoznaj.state == UIGestureRecognizerState.began { //opet sam dobijao dve iste vrednosti za jedan dugi klik pa sam uveo ovo da se zna da mi treba jedna vrednost
+            let tacka: CGPoint = prepoznaj.location(in: self.tableView)
+            let mojIndeks = tableView.indexPathForRow(at: tacka)!
+            let mojaTačka: String = "\(mojIndeks)"
+            niz1 = mojaTačka.components(separatedBy: "{length = 2, path = 0 - ")
+            niz2 = self.niz1[1].components(separatedBy: "}")
+            let indeks = Int(niz2[0])
+            let alert: UIAlertController = UIAlertController(title: "Potvrdite brisanje!", message: "Obrisati izabran red?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Da", style: .destructive, handler: { (UIAlertAction) -> Void in
+                if lokacije.count > 0 {
+                    lokacije.remove(at: indeks!)
+                }
+                nizGeografskihDužina.remove(at: indeks!)
+                UserDefaults.standard.set(nizGeografskihDužina, forKey: "cuvajDuzine")
+                nizGeografskihŠirina.remove(at: indeks!)
+                UserDefaults.standard.set(nizGeografskihŠirina, forKey: "cuvajSirine")
+                nizAdresaUlica.remove(at: indeks!)
+                UserDefaults.standard.set(nizAdresaUlica, forKey: "cuvajUlice")
+                nizAdresaBrojeva.remove(at: indeks!)
+                UserDefaults.standard.set(nizAdresaBrojeva, forKey: "cuvajBrojeve")
+                self.tableView.reloadData()
+                //NSUserDefaults.standardUserDefaults().setObject(lokacije, forKey: "lokacije")
+            }))
+            alert.addAction(UIAlertAction(title: "Ne", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "trazi" {
+            izabraniRedTabele = nil
+        }
+        
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    override func viewWillAppear(_ animated: Bool) { // ovo sam dodao da bi radila tabela
+        if UserDefaults.standard.object(forKey: "cuvajDuzine") != nil {
+            nizGeografskihDužina = UserDefaults.standard.object(forKey: "cuvajDuzine") as! [String]
+        }
+        if UserDefaults.standard.object(forKey: "cuvajSirine") != nil {
+            nizGeografskihŠirina = UserDefaults.standard.object(forKey: "cuvajSirine") as! [String]
+        }
+        if UserDefaults.standard.object(forKey: "cuvajUlice") != nil {
+            nizAdresaUlica = UserDefaults.standard.object(forKey: "cuvajUlice") as! [String]
+        }
+        if UserDefaults.standard.object(forKey: "cuvajBrojeve") != nil {
+            nizAdresaBrojeva = UserDefaults.standard.object(forKey: "cuvajBrojeve") as! [String]
+        }
+        if lokacije.count > 0 {
+            for i in 0...lokacije.count - 1 {
+                nizGeografskihDužina.append("\(lokacije[i].geografskaDužina)")
+                UserDefaults.standard.set(nizGeografskihDužina, forKey: "cuvajDuzine")
+                nizGeografskihŠirina.append("\(lokacije[i].geografskaŠirina)")
+                UserDefaults.standard.set(nizGeografskihŠirina, forKey: "cuvajSirine")
+                nizAdresaUlica.append("\(lokacije[i].ulica)")
+                UserDefaults.standard.set(nizAdresaUlica, forKey: "cuvajUlice")
+                nizAdresaBrojeva.append("\(lokacije[i].broj)")
+                UserDefaults.standard.set(nizAdresaBrojeva, forKey: "cuvajBrojeve")
+                
+            }
+        }
+        print(nizGeografskihDužina)
+        print(nizGeografskihŠirina)
+        print(nizAdresaUlica)
+        print(nizAdresaBrojeva)
+        tableView.reloadData()
     }
-    */
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // Override to support conditional editing of the table view.
+     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+     if editingStyle == .Delete {
+     // Delete the row from the data source
+     tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+     } else if editingStyle == .Insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
 
 }
--- opet moje lokacije/ViewController.swift
+++ opet moje lokacije/ViewController.swift
@@ -7,19 +7,166 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class PosećeneLokacije{
+    var geografskaDužina: CLLocationDegrees
+    var geografskaŠirina: CLLocationDegrees
+    var ulica: String
+    var broj: String
+    init(geografskaDužina: CLLocationDegrees, geografskaŠirina: CLLocationDegrees, ulica: String, broj: String){
+        self.geografskaDužina = geografskaDužina
+        self.geografskaŠirina = geografskaŠirina
+        self.ulica = ulica
+        self.broj = broj
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+var lokacije = [PosećeneLokacije]()
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapa: MKMapView!
+    let menadzerLokacije = CLLocationManager()
+    
+    override func viewDidLoad() {
+        //if NSUserDefaults.standardUserDefaults().objectForKey("lokacije") != nil {
+        //lokacije = NSUserDefaults.standardUserDefaults().objectForKey("lokacije") as! [PosećeneLokacije]
+        //}
+        super.viewDidLoad()
+        menadzerLokacije.delegate = self
+        menadzerLokacije.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if izabraniRedTabele == nil {
+            menadzerLokacije.requestWhenInUseAuthorization()
+            menadzerLokacije.startUpdatingLocation()
+        }
+        if izabraniRedTabele != nil{
+            
+            let gDužina: CLLocationDegrees = CLLocationDegrees(nizGeografskihDužina[izabraniRedTabele!])!
+            let gŠirina: CLLocationDegrees = CLLocationDegrees(nizGeografskihŠirina[izabraniRedTabele!])!
+            let mojaLokacija: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: gDužina, longitude: gŠirina)
+            
+            let zumGDužina: CLLocationDegrees = 0.01
+            let zumGVisina: CLLocationDegrees = 0.01
+            let zumiranje: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: zumGDužina, longitudeDelta: zumGVisina)
+            
+            let region: MKCoordinateRegion = MKCoordinateRegion(center: mojaLokacija, span: zumiranje)
+            mapa.setRegion(region, animated: true)
+            
+            let ulicaLokaliteta = nizAdresaUlica[izabraniRedTabele!]
+            let brojLokaliteta = nizAdresaBrojeva[izabraniRedTabele!]
+            let marker = MKPointAnnotation()
+            marker.coordinate = mojaLokacija
+            marker.title = "\(ulicaLokaliteta) \(brojLokaliteta)"
+            self.mapa.addAnnotation(marker) //zavrsio sam sa pravljenjem markera
+            
+        }
+        
+        let dugiStisak = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.pritisak(_:)))
+        mapa.addGestureRecognizer(dugiStisak)
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let korisnik: CLLocation = locations[0] 
+        let gDužina: CLLocationDegrees = korisnik.coordinate.latitude
+        let gŠirina: CLLocationDegrees = korisnik.coordinate.longitude
+        let lokacija: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: gDužina, longitude: gŠirina)
+        
+        let zumGDužina: CLLocationDegrees = 0.01
+        let zumGVisina: CLLocationDegrees = 0.01
+        let zumiranje: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: zumGDužina, longitudeDelta: zumGVisina)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: lokacija, span: zumiranje)
+        mapa.setRegion(region, animated: true)
+        
+        
+        let marker = MKPointAnnotation()
+        CLGeocoder().reverseGeocodeLocation(korisnik, completionHandler: { (mojaMesta, greska) in
+            var ulicaLokaliteta = ""
+            var brojLokaliteta = ""
+            if greska != nil {
+                print("Nešto nije u redu sa lokacijom")
+            }
+            else{
+                if let lokacije = mojaMesta?[0]{
+                    if lokacije.subThoroughfare != nil {
+                        brojLokaliteta = lokacije.subThoroughfare!
+                    }
+                    if lokacije.thoroughfare != nil {
+                        ulicaLokaliteta = lokacije.thoroughfare!
+                    }
+                }
+            }//ovde sam završio sa onim za šta mi Geocoder treba, a dalje koristim kložer da napravim marker i popunim tabelu
+            
+            
+            
+            marker.coordinate = lokacija
+            if ulicaLokaliteta == "" && brojLokaliteta == ""{
+                marker.title = String(describing: Date()) // ukoliko ne može da uhvati najbližu adresu, ispisaće datum kad je marker postavljen
+            }
+            else{
+                marker.title = "\(ulicaLokaliteta) \(brojLokaliteta)"
+            }
+            self.mapa.addAnnotation(marker) //zavrsio sam sa pravljenjem markera
+            
+        })
+    }
+    
+    var adresaBroj = ""
+    var adresaUlica = ""
+    var koordinata: CLLocationCoordinate2D?
+    
+    func pritisak(_ stisak: UILongPressGestureRecognizer){
+        if stisak.state == UIGestureRecognizerState.began { // ovo je ubačeno jer je prilikom testiranja na jedan stisak 2X printovao STISNUTO
+            stisak.minimumPressDuration = 2
+            //print("Stisnuto")
+            let pritisnutaTačka = stisak.location(in: mapa)
+            //print("pritisnuta je tačka sa koordinatama X = \(pritisnutaTačka.x) i Y = \(pritisnutaTačka.y)")
+            koordinata = mapa.convert(pritisnutaTačka, toCoordinateFrom: mapa)
+            //print("pritisnuta je tačka sa geografskim koordinatama X = \(koordinata.latitude) i Y = \(koordinata.longitude)")
+            let novaLokacija = CLLocation(latitude: koordinata!.latitude, longitude: koordinata!.longitude) // zato što Geocoder ne prihvata ni promenljivu KOORDINATA, ni CLLocationCoordinate2D, već isključivo CLLocation
+            
+            //pomoću Geocodera dobijam najbližu adresu, ali ću u njegovom kložeru ispisati i kod za ubacivanje novih lokacija u tabelu
+            
+            CLGeocoder().reverseGeocodeLocation(novaLokacija, completionHandler: { (mojaMesta, greska) in
+                
+                if greska != nil {
+                    print("Nešto nije u redu sa lokacijom")
+                }
+                else{
+                    if let lokacije = mojaMesta?[0]{
+                        if lokacije.subThoroughfare != nil {
+                            self.adresaBroj = lokacije.subThoroughfare!
+                        }
+                        if lokacije.thoroughfare != nil {
+                            self.adresaUlica = lokacije.thoroughfare!
+                        }
+                    }
+                }//ovde sam završio sa onim za šta mi Geocoder treba, a dalje koristim kložer da napravim marker i popunim tabelu
+                
+                
+                let marker = MKPointAnnotation()
+                marker.coordinate = self.koordinata!
+                if self.adresaBroj == "" && self.adresaUlica == ""{
+                    marker.title = String(describing: Date()) // ukoliko ne može da uhvati najbližu adresu, ispisaće datum kad je marker postavljen
+                }
+                else{
+                    marker.title = "\(self.adresaUlica) \(self.adresaBroj)"
+                }
+                self.mapa.addAnnotation(marker) //zavrsio sam sa pravljenjem markera
+                
+                
+                
+                lokacije.append(PosećeneLokacije(geografskaDužina: self.koordinata!.latitude, geografskaŠirina: self.koordinata!.longitude, ulica: self.adresaUlica, broj: self.adresaBroj))
+                
+            })
+        }
+        
+        
+    }
+    
+}
+//dodao bzvz
